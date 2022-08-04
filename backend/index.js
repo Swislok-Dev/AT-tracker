85ca7aa1b73c061fb4278029e35ba691ea98f232
Swislok-Dev <cmarker4@gmail.com>
2022-08-04 08:26:13 -0500

    setup entry point for express server

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+require('dotenv').config()
+require('colors')
+const port = process.env.PORT
+const { errorHandler } = require('./middleware/errorMiddleware')
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/flights', require('./routes/flightsRoutes'))
+
+app.use(errorHandler)
+
+app.listen(port, () => console.log(`Server started on port ${port}`.magenta))
