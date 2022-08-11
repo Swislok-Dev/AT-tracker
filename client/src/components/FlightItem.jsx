bc02dd3a95a45d3b696ffd411f6d9c2f3b56e2b5
Swislok-Dev <cmarker4@gmail.com>
2022-08-11 18:39:51 -0500

    add departure/arrival headings for both cards

--- client/src/components/FlightItem.jsx
+++ client/src/components/FlightItem.jsx
@@ -10,7 +10,11 @@ export default function FlightItem(props) {
       return newDate;
     }
   };
-  console.log(convertDate(nextFlight.scheduled_off));
+
+  const placeDate = () => {
+    let date = new Date();
+    return convertDate(date);
+  };
 
   return (
     <>
@@ -23,56 +27,64 @@ export default function FlightItem(props) {
           </h2>
 
           <h3>{nextFlight.status}</h3>
-
+          <h4>{placeDate()} UTC</h4>
         </div>
 
         <div className="flight-grid-item">
-          <table>
-            <thead>DEPARTURE</thead>
-            <tr>
-              <td>Airport</td>
-              <td>{nextFlight.origin.code_iata}</td>
-            </tr>
-            <tr>
-              <td>Scheduled Takeoff</td>
-              <td>{convertDate(nextFlight.scheduled_off)}</td>
-            </tr>
-            <tr>
-              <td>Actual Takeoff</td>
-              <td>{convertDate(nextFlight.actual_off)}</td>
-            </tr>
-            <tr>
-              <td>Terminal</td>
-              <td>{nextFlight.terminal_origin}</td>
-            </tr>
-            <tr>
-              <td>Gate</td>
-              <td>{nextFlight.gate_origin}</td>
-            </tr>
+          <h3 className="departure-heading">Departure</h3>
+          <h3 className="arrival-heading">Arrival</h3>
+          <table className="departure-table">
+            <tbody>
+              <tr>
+                <td>Airport</td>
+                <td>{nextFlight.origin.code_iata}</td>
+              </tr>
+              <tr>
+                <td>Scheduled Takeoff</td>
+                <td>{convertDate(nextFlight.scheduled_off)}</td>
+              </tr>
+              <tr>
+                <td>Actual Takeoff</td>
+                <td>{convertDate(nextFlight.actual_off)}</td>
+              </tr>
+              <tr>
+                <td>Terminal</td>
+                <td>{nextFlight.terminal_origin}</td>
+              </tr>
+              <tr>
+                <td>Gate</td>
+                <td>{nextFlight.gate_origin}</td>
+              </tr>
+            </tbody>
           </table>
 
-          <table>
-            <thead>ARRIVAL</thead>
-            <tr>
-              <td>Airport</td>
-              <td>{nextFlight.destination.code_iata}</td>
-            </tr>
-            <tr>
-              <td>Scheduled Arrival</td>
-              <td>{convertDate(nextFlight.scheduled_on)}</td>
-            </tr>
-            <tr>
-              <td>Actual Arrival</td>
-              <td>{convertDate(nextFlight.actual_on)}</td>
-            </tr>
-            <tr>
-              <td>Terminal</td>
-              <td>{nextFlight.terminal_destination}</td>
-            </tr>
-            <tr>
-              <td>Gate</td>
-              <td>{nextFlight.gate_destination}</td>
-            </tr>
+          <table className="arrival-table">
+            <tbody>
+              <tr>
+                <td>Airport</td>
+                <td>{nextFlight.destination.code_iata}</td>
+              </tr>
+              <tr>
+                <td>Scheduled Arrival</td>
+                <td>{convertDate(nextFlight.scheduled_on)}</td>
+              </tr>
+              <tr>
+                <td>Estimated Arrival</td>
+                <td>{convertDate(nextFlight.estimated_on)}</td>
+              </tr>
+              <tr>
+                <td>Actual Arrival</td>
+                <td>{convertDate(nextFlight.actual_on)}</td>
+              </tr>
+              <tr>
+                <td>Terminal</td>
+                <td>{nextFlight.terminal_destination}</td>
+              </tr>
+              <tr>
+                <td>Gate</td>
+                <td>{nextFlight.gate_destination}</td>
+              </tr>
+            </tbody>
           </table>
         </div>
       </div>
