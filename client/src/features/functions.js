f80df8d997beef655772760032f89386fe5fd41a
Swislok-Dev <cmarker4@gmail.com>
2022-08-11 18:43:44 -0500

    clean up comments/logs

--- client/src/features/functions.js
+++ client/src/features/functions.js
@@ -10,26 +10,14 @@ export const findNextFlight = (flightsArray) => {
   for (let i = 0; i < flightsArray.length; i++) {
     let flightTime = convertISOTimeToDateTime(flightsArray[i].scheduled_off);
     let nextFlight = flightsArray[i + 1];
-    if (flightTime > now.setDate(9) && nextFlight.status !== "Scheduled") {
+    if (flightTime > now && nextFlight.status !== "Scheduled") {
       if (nextFlight.status.includes("En Route")) {
         currentFlight = nextFlight;
         return currentFlight;
       }
       currentFlight = flightsArray[i];
     } else {
-      console.log(`Flight status:${[i]} ${flightsArray[i].status}`);
     }
   }
-  console.log(currentFlight);
   return currentFlight;
 };
-
-// export const hasLeft = (flight) => {
-//   const now = new Date()
-//   const flightTime = new Date(flight.scheduled_off)
-//   if (flightTime <  now) {
-//     console.log("Flight has left")
-//   } else {
-//     console.error("Flight hasn't left")
-//   }
-// }
