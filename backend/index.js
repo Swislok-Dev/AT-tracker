4d2f08db4192e3e9826616cf6f89171db765a19c
Swislok-Dev <cmarker4@gmail.com>
2022-08-14 18:47:49 -0500

    prepare for heroku deployment

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,30 @@
-const express = require('express')
-require('dotenv').config()
-require('colors')
-const port = process.env.PORT
-const { errorHandler } = require('./middleware/errorMiddleware')
+const express = require("express");
+const path = require("path");
+require("dotenv").config();
+require("colors");
+const port = process.env.PORT;
+const { errorHandler } = require("./middleware/errorMiddleware");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/flights', require('./routes/flightsRoutes'))
+app.use("/api/flights", require("./routes/flightsRoutes"));
 
-app.use(errorHandler)
+// Serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 
-app.listen(port, () => console.log(`Server started on port ${port}`.magenta))
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "client", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
+
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}`.magenta));
