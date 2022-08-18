2c18f332da8c49f75d3cb1b1311d648c045883dd
Swislok-Dev <cmarker4@gmail.com>
2022-08-18 09:05:16 -0500

    remove flight ident from showing in window

--- client/src/components/FlightsList.jsx
+++ client/src/components/FlightsList.jsx
@@ -6,8 +6,6 @@ function FlightsList(props) {
 
   return (
     <>
-      {/*TODO: place this in a function for flight number display in sub-header*/}
-      {props.nextFlight.ident}
       <br/>
       <FlightItem nextFlight={nextFlight} />
 
